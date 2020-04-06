57c6bb0a929f1d7ffcd357526ce3699a77895dd1
Kryštof Matějka <kx.matejka@gmail.com>
2020-04-06 21:23:11 +0200

    feat: load proposals from api

--- app/lib/request.js
+++ app/lib/request.js
@@ -1,16 +1,18 @@
-export const requestGet = ({url}) => request({url, method: 'GET'})
+export const requestGet = ({url, bearerToken}) => request({url, bearerToken, method: 'GET'})
 
-export const requestPost = ({url, payload}) => request({url, payload, method: 'POST'})
+export const requestPost = ({url, payload, bearerToken}) => request({url, payload, bearerToken, method: 'POST'})
 
-const request = ({url, method, payload}) => {
+const request = ({url, payload, bearerToken, method}) => {
   const serializedPayload = (typeof payload === 'object') ? JSON.stringify(payload) : ''
   const contentTypeHeader = (serializedPayload.length) ? {'Content-Type': 'application/json'} : {}
+  const bearerTokenHeader = (typeof bearerToken === 'string') ? {'Authorization': `Bearer ${bearerToken}`} : {}
 
   return fetch(url, {
     method: method,
     headers: {
       Accept: 'application/json',
-      ...contentTypeHeader
+      ...contentTypeHeader,
+      ...bearerTokenHeader
     },
     body: serializedPayload
   }).then(response => response.json())
--- app/screens/VoteSabatProposal/index.js
+++ app/screens/VoteSabatProposal/index.js
@@ -1,6 +1,9 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import {QuadraticVotingButton} from './QuadraticVotingButton'
+import {requestGet} from '../../lib'
+import {API} from '../../constants'
+import {useIdentity} from '../../context'
 
 export const Container = styled.View`
   flex: 1;
@@ -19,46 +22,81 @@ const VotesStatus = styled.Text`
   margin: 10px;
 `
 
+const STATUS = {
+  loading: 'LOADING',
+  error: 'ERROR',
+  done: 'DONE'
+}
+
+const fetchProposals = (sabatId, token) => requestGet({
+  url: API.sabatsProposals.replace(':id', sabatId),
+  bearerToken: token
+})
+
+const validateResponse = (response) => {
+  if (typeof response !== 'object') {
+    throw new Error('Response must be an object')
+  }
+
+  if (!Array.isArray(response.data)) {
+    throw new Error('Data must be an array')
+  }
+
+  return response
+}
+
+const parseProposals = (data) => data.map(record => ({
+  id: record.ID,
+  text: record.description,
+  value: 0
+}))
+
 export default function VoteSabatProposal() {
+  const [status, setStatus] = React.useState(STATUS.loading)
   const [votes, setVotes] = React.useState(100)
-  const [proposals, setProposals] = React.useState([
-    {
-      id: 1,
-      value: 0,
-      text: 'Konzerva pro každého hladového afričana'
-    },
-    {
-      id: 2,
-      value: 0,
-      text: 'Deka pro každého bezdomovce'
-    },
-    {
-      id: 3,
-      value: 0,
-      text: 'Legalizace heroinu'
-    }
-  ])
+  const [proposals, setProposals] = React.useState([])
+  const {identity} = useIdentity()
+
+  React.useEffect(() => {
+    fetchProposals(1, identity.token)
+      .then(response => validateResponse(response))
+      .then(response => parseProposals(response.data))
+      .then(proposals => {
+        setProposals(proposals)
+        setStatus(STATUS.done)
+      })
+      .catch(error => {
+        console.log(error)
+        setStatus(STATUS.error)
+      })
+  }, [])
 
   return (
     <Container>
-      <VotesStatus>Zbývajících hlasů: {votes}</VotesStatus>
-      <Content>
-        {
-          proposals.map(proposal => <QuadraticVotingButton
-            key={proposal.id}
-            votes={votes}
-            value={proposal.value}
-            text={proposal.text}
-            handleOnChange={({newVotes, newProposalValue}) => {
-              setVotes(newVotes)
-              setProposals([...proposals].map(newProposal => newProposal.id !== proposal.id ? ({...newProposal}) : ({
-                ...newProposal,
-                value: newProposalValue
-              })))
-            }}
-          />)
-        }
-      </Content>
+      {status === 'LOADING' && <VotesStatus>načítám...</VotesStatus>}
+      {status === 'ERROR' && <VotesStatus>načítání se nezdařilo</VotesStatus>}
+      {status === 'DONE' &&
+        <>
+          <VotesStatus>Zbývajících hlasů: {votes}</VotesStatus>
+          <Content>
+            {
+              proposals.map(proposal => <QuadraticVotingButton
+                key={proposal.id}
+                votes={votes}
+                value={proposal.value}
+                text={proposal.text}
+                handleOnChange={({newVotes, newProposalValue}) => {
+                  setVotes(newVotes)
+                  setProposals(proposals.map(newProposal => newProposal.id !== proposal.id ? ({...newProposal}) : ({
+                    ...newProposal,
+                    value: newProposalValue
+                  })))
+                }}
+              />)
+            }
+          </Content>
+        </>
+      }
     </Container>
   )
 }
