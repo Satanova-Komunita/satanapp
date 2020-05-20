9aa03d35f85b270accd474e9e15a02481162daaf
Kryštof Matějka <kx.matejka@gmail.com>
2020-05-20 22:32:04 +0200

    refactor: improve identity state management

--- packages/native/context/identity.tsx
+++ packages/native/context/identity.tsx
@@ -7,42 +7,61 @@ interface Identity {
   status: 'INITIALIZING'|'SIGNED_OUT'|'SIGNED_IN'
 }
 
-interface IdentityContextValue {
-  identity: Identity,
-  setIdentity: (token: string, memberNumber: number) => void,
-  resetIdentity: () => void
-}
-
 const EMPTY_IDENTITY: Identity = {
   token: '',
   memberNumber: 0,
   status: 'INITIALIZING'
 }
 
+type ActionPayload = {
+  token: string,
+  memberNumber: number
+}
+
+type Action =
+  | {type: 'SIGN_IN', payload: ActionPayload}
+  | {type: 'SIGN_OUT' }
+
+const reducer = (state: Identity, action: Action): Identity => {
+  switch (action.type) {
+    case 'SIGN_IN':
+      return {
+        token: action.payload.token,
+        memberNumber: action.payload.memberNumber,
+        status: 'SIGNED_IN'
+      }
+    case 'SIGN_OUT':
+      return {
+        ...EMPTY_IDENTITY,
+        status: 'SIGNED_OUT'
+      }
+  }
+}
+
 const STORAGE_KEY = 'identity:last'
 
-const IdentityContext = React.createContext<IdentityContextValue>({
+const IdentityContext = React.createContext({
   identity: EMPTY_IDENTITY,
-  setIdentity: () => {},
+  setIdentity: (token: string, memberNumber: number) => {},
   resetIdentity: () => {}
 })
 
 IdentityContext.displayName = 'IdentityContext'
 
 const IdentityProvider: React.FunctionComponent = ({children}) => {
-  const [identity, setIdentity] = React.useState<Identity>(EMPTY_IDENTITY)
+  const [identity, dispatch] = React.useReducer(reducer, EMPTY_IDENTITY)
 
   React.useEffect(() => {
     storageGet(STORAGE_KEY)
       .then((result: any) => {
         if (result === null) {
-          return setIdentity({...EMPTY_IDENTITY, status: 'SIGNED_OUT'})
+          return dispatch({type: 'SIGN_OUT'})
         }
-        return setIdentity({
+
+        return dispatch({type: 'SIGN_IN', payload: {
           token: result.token,
           memberNumber: result.memberNumber,
-          status: 'SIGNED_IN'
-        })
+        }})
       })
       .catch((error) => console.error('Error while initializing identity', error))
   }, [])
@@ -50,23 +69,19 @@ const IdentityProvider: React.FunctionComponent = ({children}) => {
   return (
     <IdentityContext.Provider value={{
       identity,
-      setIdentity: (token: string, memberNumber: number) => {
-        return storageSet(STORAGE_KEY, {token, memberNumber}).then(() => {
-          setIdentity({token, memberNumber, status: 'SIGNED_IN'})
-        })
-      },
-      resetIdentity: () => {
-        return removeItem(STORAGE_KEY).then(() => {
-          setIdentity({...EMPTY_IDENTITY, status: 'SIGNED_OUT'})
-        })
-      }
+      setIdentity: (token: string, memberNumber: number) => storageSet(STORAGE_KEY, {token, memberNumber})
+        .then(() => dispatch({type: 'SIGN_IN', payload: {
+            token,
+            memberNumber,
+          }})),
+      resetIdentity: () => removeItem(STORAGE_KEY).then(() => dispatch({type: 'SIGN_OUT'}))
     }}>
       {children}
     </IdentityContext.Provider>
   )
 }
 
-const useIdentity = () => React.useContext<IdentityContextValue>(IdentityContext)
+const useIdentity = () => React.useContext(IdentityContext)
 
 export {
   IdentityProvider,
